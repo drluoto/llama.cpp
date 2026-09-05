0bdda8d5caa5a878fbae78a1cb0e748aa755fca5
M5 merge <jl@lokal>
2026-09-05 08:12:16 +0200

    re-apply GDN l2-norm fix (PR 28068) after reverting #27880

--- src/models/qwen4exp.cpp
+++ src/models/qwen4exp.cpp
@@ -1428,10 +1428,11 @@ ggml_tensor * llama_model_qwen4exp::graph::build_layer_attn_linear(
     cb(k_conv, "k_conv", il);
     cb(v_conv, "v_conv", il);
 
+
     const float eps_norm = hparams.f_norm_rms_eps;
 
-    q_conv = ggml_l2_norm(ctx0, q_conv, eps_norm);
-    k_conv = ggml_l2_norm(ctx0, k_conv, eps_norm);
+    q_conv = build_gdn_l2_norm(ctx0, q_conv, eps_norm);
+    k_conv = build_gdn_l2_norm(ctx0, k_conv, eps_norm);
 
 
 
